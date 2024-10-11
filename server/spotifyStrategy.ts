d73740fd7cb478c79cb296be5471f1bb1dd15ef8
Johanna <hl105@wellesley.edu>
2024-10-11 02:58:25 -0400

    vercel debug

--- server/spotifyStrategy.ts
+++ server/spotifyStrategy.ts
@@ -26,7 +26,7 @@ passport.use(
       showDialog: true,
       clientID: clientID,
       clientSecret: clientSecret,
-      callbackURL: "http://localhost:3000/api/spotify/callback/",
+      callbackURL: "https://backend-starter-steel.vercel.app/api/spotify/callback",
       scope: ["user-read-private", "user-read-currently-playing", "user-read-playback-state", "user-modify-playback-state"],
     },
     async (accessToken: string, refreshToken: string, expires_in: number, profile: Profile, done: Function) => {
